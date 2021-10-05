645b3057e592205945cd7d048b804d6bbae7bdcb
Student <mohamedelhakim7@gmail.com>
2021-10-05 10:17:28 +0100

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *  main - putchar
- * 
+ *
  * Return: 0
  */
 int main(void)
 {
-        int n;
-
-        sprintf("_putchar"\n)
-return (0)        
+int n;
+putchar("_putchar")
+putchar('\n');
+return (0);
 }
