a8a7f23c062811fd624f857dba07537343e1f1de
Student <mohamedelhakim7@gmail.com>
2021-10-05 13:32:49 +0100

    printAlphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * sprint_alphabet_x10 - unction that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet_x10(void);
+{
+char ch;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar (ch);
+putchar ('\n');
+}
+}
