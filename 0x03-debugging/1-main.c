d8d4ca6d3d566559a40c686438820c070c70b41e
Student <mohamedelhakim7@gmail.com>
2021-10-07 10:26:05 +0100

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :""\n");
+printf("Infinite loop incoming :(""\n");
 
 /**
 * i = 0;
@@ -15,6 +15,6 @@ printf("Infinite loop incoming :""\n");
 * putchar(i);
 *   }
 */
-printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/""\n");
 return (0);
 }
