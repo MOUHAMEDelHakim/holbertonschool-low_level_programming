bcd73810e2de297115c44060c993cb2708e6ced3
Student <mohamedelhakim7@gmail.com>
2021-10-11 12:34:20 +0100

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - a function that prints a string, followed by a new line, to stdout
+ *
+ *@str: a function that prints a string, followed by a new line, to stdout
+ */
+void _puts(char *str)
+{
+sprintf("%s, str")
+sprintf('\n')
+}
