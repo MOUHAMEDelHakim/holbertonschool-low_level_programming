07ad4085b5651bd8c3484d9015c8903397401289
Student <mohamedelhakim7@gmail.com>
2021-10-11 13:29:19 +0100

    PrintRev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts -  a function that prints a string, in reverse.
+ * int - i
+ * @str: int
+ * Return: i
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+return (i);
+}
+{
+putchar(s[i - 1])
+putchar("\n")
+i--;
+}
