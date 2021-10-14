c42eac031afc8c6524251543bcf88de3d278239b
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2021-10-14 09:09:48 -1100

    Main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#include <stdio.h>
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <main.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +9,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *)
 char *leet(char *);
+#endif /* MAIN_H */
