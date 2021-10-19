1515a8fd983ad8484b4c64ad0f46f7562f2a817d
Student <mohamedelhakim7@gmail.com>
2021-10-19 10:43:32 +0100

    mmset*

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset -  function fills the first n bytes of the memory area pointed
+ * @s: Returns a pointer to the memory area s
+ * @n: unction fills the first n bytes of the memory area pointed
+ * @b : The character to fill the memory area with.
+ *  Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++]
+s[i] = b;
+return (s);
+}
