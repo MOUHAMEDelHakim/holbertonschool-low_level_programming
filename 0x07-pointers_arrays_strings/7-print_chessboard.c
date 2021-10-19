634ac9aaa3c6957aeef83741dccb0c1e72f8eecd
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2021-10-19 09:51:18 -1100

    ChessB

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - Print a chessb.
+ * @a: chessb
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; a[i][7]; i++)
+{
+for (j = 0; j < 8; j++)
+putchar(a[i][j]);
+
+putchar('\n');
+}
+}
