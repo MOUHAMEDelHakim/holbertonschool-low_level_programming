778486dffb9b0252ca1deaf04401fe77d49653f6
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2021-10-20 01:14:04 -1100

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,17 @@
 /**
  * _puts_recursion - a function that prints a string
  *@s: string to print
- * Return: 0
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
+{
 if (*s == '\0')
 {
-_putchar('\n')
+_putchar('\n');
+return;
 }
-putchar(*s);
+_putchar(*s);
 _puts_recursion(s + 1);
-return (0);
 }
