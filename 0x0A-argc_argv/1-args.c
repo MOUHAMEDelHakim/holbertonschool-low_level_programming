cb761e1f926829e859c2d6ba9bcd2ef391315edf
Student <mohamedelhakim7@gmail.com>
2021-10-22 13:03:24 +0100

    ArgS

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program should print a number
+ * @argc: int
+ * @argv: int
+ * return: int
+ */
+int main(__attribute__((unused))int argc, char** argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
