ee87c637d0387157937a0093a2af5f16dee395af
Student <mohamedelhakim7@gmail.com>
2021-10-22 13:55:35 +0100

    argS

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,8 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-int counter;
-for (counter = 0; counter < argc; counter++)
-printf("argv[%2d]: %s\n", counter, argv[counter]);
+int arg;
+for (arg = 0; arg < argc ; arg++)
+printf("%s\n", argv[arg]);
 return (0);
 }
