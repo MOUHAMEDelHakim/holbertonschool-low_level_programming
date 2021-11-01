4b0d3d47e7c1e672291a7543f2b336ef9d28468c
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2021-11-01 02:27:42 -1100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGH
+#define DOGH
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
