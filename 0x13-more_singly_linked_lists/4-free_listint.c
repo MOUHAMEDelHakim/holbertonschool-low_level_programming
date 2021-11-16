ccf5e8193b02328f6ce21fbb13bccc695aa2469a
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2021-11-16 11:18:22 -1100

    Free List

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint -  a function that frees a listint_t list.
+ * @head: a variable pointing to a struct of type listint_t.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
