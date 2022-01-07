6df9e45c7213bca3f0c2bdfb7f0f4fbff7b79330
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2022-01-07 13:17:53 +0100

    Dlistinst

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,18 +3,20 @@
 #include "lists.h"
 
 /**
- * dlistintlen - returns the nmb of elements
- * @h: the pointer.
- * return: the nmb of elements
+ * dlistint_len - returns the number of elements.
+ * @h: a pointer.
+ * Return: number of elements.
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t a;
-    a = 0;
-    while (h !=NULL)
-    {
-    a = a + 1;
-    h = h->next;
-    }
-    return (a);
+	size_t a;
+
+	a = 0;
+	while (h != NULL)
+	{
+		a = a + 1;
+		h = h->next;
+	}
+	return (a);
 }
