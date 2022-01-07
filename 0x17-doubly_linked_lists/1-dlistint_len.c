3694058a361a53fce3c4090271740b2703ecf40f
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2022-01-07 13:09:51 +0100

    Dlistint

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistintlen - returnts the nmb of elements
+ * @h: the pointer.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t a;
+    a = 0;
+    while (h !=NULL)
+    {
+        a = a + 1;
+        h = h->next;
+    }
+    return (a);
+}
+   
