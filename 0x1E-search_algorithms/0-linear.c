c3d9dbb1f733bd05de9fd81c85ed9252dd1c5c58
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2022-05-05 11:04:27 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,22 +5,22 @@
  *@array: a pointer to the first element of array to search in.
  *@size: the number of elements in array
  *@value: the value to search.
- *Return: If value is not present in array or if array is NULL, your function must return -1
+ *Return: in array or if array is NULL, your function must return -1
  */
 int linear_search(int *array, size_t size, int value)
 
 {
-    size_t i;
-    
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    for (i = 0; i < size; i++)
-    {
-        printf("value checked array [%ld] = [%d]\n" , i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return(-1);
+size_t i;
+
+if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("value checked array [%ld] = [%d]\n" , i, array[i]);
+		if (array[i] == value)
+	return (i);
+	}
+	return (-1);
 }
